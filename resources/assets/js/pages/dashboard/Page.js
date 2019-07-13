0ef8f5edc7fdbbc8717cdc46398ecab6147fe12b
Nemanja <WorldHero2018@hotmail.com>
2019-07-13 16:46:36 +0200

    remove pageheader in admin dashboard

--- resources/assets/js/pages/dashboard/Page.js
+++ resources/assets/js/pages/dashboard/Page.js
@@ -9,8 +9,6 @@ import {
     Segment,
     Step
 } from 'semantic-ui-react'
-import PageHeader from '../../common/pageHeader'
-
 class Page extends React.Component {
     constructor(props) {
         super(props);
@@ -19,8 +17,7 @@ class Page extends React.Component {
     render() {
         return (
             <div>
-                <PageHeader heading="Dashboard"/>
-                <Segment vertical textAlign='center' style={{minHeight: '100vh'}}>
+                <Segment vertical textAlign='center' style={{minHeight: '110vh', paddingTop: 70}}>
                     <Header as='h1'>Dashboard</Header>
                 </Segment>
             </div>
