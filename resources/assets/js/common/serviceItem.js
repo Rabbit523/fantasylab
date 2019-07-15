279684d8429145e78d6656a1b345026facca020f
Nemanja <WorldHero2018@hotmail.com>
2019-07-15 21:10:51 +0200

    complete service item card component

---
+++ resources/assets/js/common/serviceItem.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Sumit-Yadav on 16-10-2017.
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+import image from '../../images/theme/desktop.png'
+class ServiceItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={`${"service-item " + this.props.type}`}>
+                <div className={`${"avatar-item " + this.props.type}`}>
+                    <img src={`${ this.props.url}`} />
+                </div>
+                <div className="text-item">
+                    <p>{this.props.title}</p>
+                </div>
+                <div className="text-description">
+                    <p>{this.props.description}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+ServiceItem.propTypes = {
+    url: PropTypes.string.isRequired,
+    type:PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+};
+export default ServiceItem;
